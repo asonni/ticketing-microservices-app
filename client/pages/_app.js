0a299d870bf35fd6b20b4391ff54b7faff50ea9e
Aladdin Sonni <aladdin.sonni@gmail.com>
2020-05-29 05:53:26 +0200

    Complete client side

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -7,24 +7,28 @@ const AppComponent = ({ Component, pageProps, currentUser }) => {
     <>
       <Header currentUser={currentUser} />
       <div className="container">
-        <Component {...pageProps} />
+        <Component currentUser={currentUser} {...pageProps} />
       </div>
     </>
   );
 };
 
-AppComponent.getInitialProps = async (appContext) => {
+AppComponent.getInitialProps = async appContext => {
   const client = buildClient(appContext.ctx);
   const { data } = await client.get('/api/users/current-user');
 
   let pageProps = {};
   if (appContext.Component.getInitialProps) {
-    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+    pageProps = await appContext.Component.getInitialProps(
+      appContext.ctx,
+      client,
+      data.currentUser
+    );
   }
 
   return {
     pageProps,
-    ...data,
+    ...data
   };
 };
 
